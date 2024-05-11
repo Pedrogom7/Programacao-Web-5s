a19f53311b39ba9eff9c4e065c0ed92225b28328
Pedro Henrique Gomes <130695589+Pedrogom7@users.noreply.github.com>
2024-05-11 15:12:46 -0300

    Criando a pagina todo

--- localstorage.js
+++ localstorage.js
@@ -31,3 +31,65 @@ if (localStorage.getItem('Visits')) {
 let texto = document.getElementById("updates")
 
 texto.textContent = `Esta página foi visitada ${numeroContador} vezes. A última visita foi: ${lastVisit}`
+
+//================= TO-DO LIST =========================
+function adicionarTarefa(textoTarefa, textoDescricao) {
+
+    var borda = document.createElement('div');
+    borda.className = "tarefas";
+    var listText = document.createElement('h2');
+    var listDescription = document.createElement('p');
+    listText.textContent = textoTarefa;
+    listDescription.textContent = textoDescricao;
+
+    borda.appendChild(listText);
+    borda.appendChild(listDescription);
+
+    var todoList = document.getElementById('todo-list');
+    todoList.appendChild(borda);
+}
+
+function salvarTarefas() {
+    var tarefas = [];
+
+    var tarefasHTML = document.querySelectorAll('.tarefas');
+    tarefasHTML.forEach(function (tarefa) {
+        var textoTarefa = tarefa.querySelector('h2').textContent;
+        var textoDescricao = tarefa.querySelector('p').textContent;
+        tarefas.push({ textoTarefa: textoTarefa, textoDescricao: textoDescricao });
+    });
+
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+}
+
+function carregarTarefas() {
+    var tarefas = JSON.parse(localStorage.getItem('tarefas'));
+
+    if (tarefas) {
+        tarefas.forEach(function (tarefa) {
+            adicionarTarefa(tarefa.textoTarefa, tarefa.textoDescricao);
+        });
+    }
+}
+
+document.getElementById('todo-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var inputTarefa = document.getElementById('input-tarefa');
+    var textoTarefa = inputTarefa.value.trim();
+    var inputDescricao = document.getElementById('input-descricao');
+    var textoDescricao = inputDescricao.value.trim();
+
+    if (textoTarefa !== '') {
+        adicionarTarefa(textoTarefa, textoDescricao);
+
+        salvarTarefas();
+
+        inputTarefa.value = '';
+        inputDescricao.value = '';
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    carregarTarefas();
+});
